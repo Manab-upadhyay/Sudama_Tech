c3601bcc32ce27861ffcd5e257b4cfc76d552da1
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2025-02-25 00:34:34 +0530

    text change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
             <rect width="20" height="16" x="2" y="4" rx="2" />
             <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
           </svg>
-          Let's Build the Future
+          Let's Build Together
         </button>
       </Link>
     </div>
